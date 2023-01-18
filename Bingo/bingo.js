4232360c3a541ed2d51bd4b7e1210ce0437458a4
Alexander <aguillenhernandez95@gmail.com>
2023-01-18 21:14:30 +0100

    Fix eslint projected errors in projects

--- Bingo/bingo.js
+++ Bingo/bingo.js
@@ -1,103 +1,105 @@
-let bingo = () => {
-let promptWithType = (str, type) => {
-  let input = prompt(str);
-  if (input !== null) {
-    if (input.trim() === "") {
-      alert("Missing arguments");
-      return promptWithType(str, type);
-    } else if (type === "number") {
-      if (/^[0-9]+$/.test(input.trim())) {
-        return +input.trim();
-      } else {
-        alert("Enter only numbers.")
+/* eslint-disable no-negated-condition */
+/* eslint-disable no-alert */
+const bingo = () => {
+  const promptWithType = (str, type) => {
+    const input = prompt(str);
+    if (input !== null) {
+      if (input.trim() === "") {
+        alert("Missing arguments");
         return promptWithType(str, type);
       }
-    } else if (type === "string") {
-      if (/^[A-Za-z\s]*$/.test(input.trim())) {
-        return input.trim();
-      } else {
+
+      if (type === "number") {
+        if (/^[0-9]+$/.test(input.trim())) {
+          return Number(input.trim());
+        }
+
+        alert("Enter only numbers.");
+        return promptWithType(str, type);
+      }
+
+      if (type === "string") {
+        if (/^[A-Za-z\s]*$/.test(input.trim())) {
+          return input.trim();
+        }
+
         alert("Enter only letters.");
         return promptWithType(str, type);
       }
+    } else {
+      return input;
     }
-  } else {
-    return input;
-  }
-}
+  };
 
-let name = promptWithType("Enter your name", "string") || "stranger";
-let players = [
-  {
-    playerName: name,
-    points: 0
-  }, {
-    playerName: "Benito Camelas",
-    points: 0
-  }, {
-    playerName: "Solomeo Paredes",
-    points: 0
-  }, {
-    playerName: "Barry Allen",
-    points: 0
-  }, {
-    playerName: "Snape",
-    points: 0,
-  }, {
-    playerName: "NoobMaster69",
-    points: 0,
-  }
-]
+  const name = promptWithType("Enter your name", "string") || "stranger";
+  const players = [
+    {
+      playerName: name,
+      points: 0,
+    },
+    {
+      playerName: "Benito Camelas",
+      points: 0,
+    },
+    {
+      playerName: "Solomeo Paredes",
+      points: 0,
+    },
+    {
+      playerName: "Barry Allen",
+      points: 0,
+    },
+    {
+      playerName: "Snape",
+      points: 0,
+    },
+    {
+      playerName: "NoobMaster69",
+      points: 0,
+    },
+  ];
 
-
-alert(`Hello, ${name}. Welcome to ISDI CODERS Bingo.
+  alert(`Hello, ${name}. Welcome to ISDI CODERS Bingo.
 You will be shown a card with 15 numbers divided into 3 rows. Each row contains 5 numbers. You can choose a new card, if the one you are shown does not suit you. At each turn a random number will be shown on the screen. If the number matches any of the numbers on the card, that number will be marked with an X. If all the numbers in a row are marked, "Line!" will be alerted. If all the numbers on the card are marked, "Bingo!" will be alerted. 
 Finally, your score will be displayed on the screen.`);
 
-alert(`The points for each game are calculated as follows.
+  alert(`The points for each game are calculated as follows.
 ((90 - turns) / 75) x 100 (without decimals)
-So if you manage to complete the game in a total of 15 turns you will have 100 points. The less turns the more points.`)
+So if you manage to complete the game in a total of 15 turns you will have 100 points. The less turns the more points.`);
 
-let game = () => {
-  
-  let showPunctuation = () => {
-  let assignRandomPoints = () => {
-  for (let i = 0; i < players.length; i++) {
-    
-    if (players[i].playerName !== name) {
-      players[i].points += Math.floor(Math.random() * 101);
-    }
-    
-    else {
-      players[i].points += punctuation;
-    }
-  }
-  }
-  
-  assignRandomPoints();
-    
-   let sortPlayersByPoints = () =>  {
-  
-  players.sort((player1, player2) => {
-    
-    if (player1.points > player2.points) {
-      return -1;
-    }
-    
-    else if (player2.points > player1.points) {
-      return 1;
-    }
-    
-    else {
-      return 0;
-    }
-  });
-}
-   
-   sortPlayersByPoints();
-    
-  let player = players.find(player => player.playerName === name);
-  
-  alert(`Your cumulative score is ${player.points} points.
+  const game = () => {
+    const showPunctuation = () => {
+      const assignRandomPoints = () => {
+        for (let i = 0; i < players.length; i++) {
+          if (players[i].playerName !== name) {
+            players[i].points += Math.floor(Math.random() * 101);
+          } else {
+            players[i].points += punctuation;
+          }
+        }
+      };
+
+      assignRandomPoints();
+
+      const sortPlayersByPoints = () => {
+        players.sort((player1, player2) => {
+          if (player1.points > player2.points) {
+            return -1;
+          }
+
+          if (player2.points > player1.points) {
+            return 1;
+          }
+
+          return 0;
+        });
+      };
+
+      sortPlayersByPoints();
+
+      const player = players.find((player) => player.playerName === name);
+
+      alert(`Your cumulative score is ${player.points} points.
   
   ${players[0].playerName} - ${players[0].points}
   
@@ -109,148 +111,157 @@ let game = () => {
   
   ${players[4].playerName} - ${players[4].points}
   
-  ${players[5].playerName} - ${players[5].points}`)
-}
+  ${players[5].playerName} - ${players[5].points}`);
+    };
 
+    const assingBingoCard = (bingoCard, message, keepCard) => {
+      bingoCard = [];
 
-let assingBingoCard = (bingoCard, message, keepCard) => {
-  bingoCard = [];
-  let randomNumber = Math.floor(Math.random() * 90) + 1;
-  
-   while (bingoCard.length < 15) {
-    let randomNumber = Math.floor(Math.random() * 90) + 1;
-    if (!bingoCard.includes(randomNumber)) {
-      bingoCard.push(randomNumber);
-    }
-  }
-  
-  message = `\n${bingoCard[0]} ${bingoCard[1]} ${bingoCard[2]} ${bingoCard[3]} ${bingoCard[4]}\n${bingoCard[5]} ${bingoCard[6]} ${bingoCard[7]} ${bingoCard[8]} ${bingoCard[9]}\n${bingoCard[10]} ${bingoCard[11]} ${bingoCard[12]} ${bingoCard[13]} ${bingoCard[14]}\n\n`;
-  
-  
-let keep = () => {
-  
-   keepCard = promptWithType(`Your card is next: ${message} Enter "yes" if you want to keep the card, "no" if you want to change the card, cancel if you want to quit the game.`, "string")
- 
-if (keepCard === null) {
-   if (confirm("Wan to exit game?")) {
-     alert("Goodbye");
-   } else {
-     return keep();
-   }                     
- } else { 
-   keepCard = keepCard.trim();
-  if (keepCard === "yes") {
-       
-      return bingoCard;
-   
-} else if (keepCard === "no") {
-  return assingBingoCard();
-} else {
-  alert("Invalid input");
-  return keep();}
-}
-}
-
-return keep();
-}
-
-
-
-
-  let bingoCard = assingBingoCard(), 
-      line1 = bingoCard.slice(0, 5),
-      line2 = bingoCard.slice(5, 10),
-      line3 = bingoCard.slice(10),
-      presentedNumbers = [],
-      changeToX = (element, index, randomNumber) => {
-        if (element === randomNumber) {
-        return "X";
-    }
-    return element;
-  },
-      showCard = () => {
-        return `\n${line1.join(" ")}\n${line2.join(" ")}\n${line3.join(" ")}`;
-      },
-      i = 1,
-      correctNumbers = bingoCard,
-      incorrectNumbers = [];
-      while (incorrectNumbers.length < 75) {
-        let randomNumber = Math.floor(Math.random() * 90) + 1;
-        if (!correctNumbers.includes(randomNumber) && !incorrectNumbers.includes(randomNumber)) {
-          incorrectNumbers.push(randomNumber);
-          }
+      while (bingoCard.length < 15) {
+        const randomNumber = Math.floor(Math.random() * 90) + 1;
+        if (!bingoCard.includes(randomNumber)) {
+          bingoCard.push(randomNumber);
+        }
       }
 
-let getRandomNumber = () => {
-    let luck = Math.random() < Math.random();
-    
-  if (luck) {
-    
-    let correctNum = [correctNumbers[Math.floor(Math.random() * correctNumbers.length)], true];
-    correctNumbers.splice(correctNumbers.indexOf(correctNum[0]), 1);
-    return correctNum;
-    
-  } else {
-    
-    let incorrectNum = [incorrectNumbers[Math.floor(Math.random() * incorrectNumbers.length)], false];
-    incorrectNumbers.splice(incorrectNumbers.indexOf(incorrectNum[0]), 1);
-    return incorrectNum;
-    
-  }
-}  
-let lineAlerted = false;
+      message = `\n${bingoCard[0]} ${bingoCard[1]} ${bingoCard[2]} ${bingoCard[3]} ${bingoCard[4]}\n${bingoCard[5]} ${bingoCard[6]} ${bingoCard[7]} ${bingoCard[8]} ${bingoCard[9]}\n${bingoCard[10]} ${bingoCard[11]} ${bingoCard[12]} ${bingoCard[13]} ${bingoCard[14]}\n\n`;
 
-let turns = 1;
-while (correctNumbers.length > 0) {
-  
-  let number = getRandomNumber();
-  alert(`Turn ${turns}:\n NUMBER ${number[0]}`)
-  if (number[1]) {
-    line1 = line1.map(element => changeToX(element, null, number[0])); 
-    line2 = line2.map(element => changeToX(element, null, number[0])); 
-    line3 = line3.map(element => changeToX(element, null, number[0])); 
-    alert(`Number ${number[0]} has been found and has therefore been marked with an X.\n${showCard()}`)
-  } else {
-    alert(`Number ${number[0]} has not been found.\n${showCard()}`);
-  } 
-  
-   if (line1.every(x => x === "X") && !lineAlerted) {
-    alert("Line!");
-    lineAlerted = true;
-  }
-  
-  if (line2.every(x => x === "X") && !lineAlerted) {
-    alert("Line!");
-    lineAlerted = true;
-  }
-  
-  if (line3.every(x => x === "X") && !lineAlerted) {
-    alert("Line!");
-    lineAlerted = true;
-  }
+      const keep = () => {
+        keepCard = promptWithType(
+          `Your card is next: ${message} Enter "yes" if you want to keep the card, "no" if you want to change the card, cancel if you want to quit the game.`,
+          "string"
+        );
 
-  
-  if (line1.concat(line2, line3).every(x => x === "X")) {
-    alert("Bingo!");
-    break;
-  }
+        if (keepCard === null) {
+          if (confirm("Wan to exit game?")) {
+            alert("Goodbye");
+          } else {
+            return keep();
+          }
+        } else {
+          keepCard = keepCard.trim();
+          if (keepCard === "yes") {
+            return bingoCard;
+          }
 
-  turns++
-  }
-  alert(`You have completed this game in a total of ${turns} turns.`)
-  let punctuation = Math.trunc(((90 - turns) / 75) * 100);
-  alert(`Your score for this round is ${punctuation} points.`)
-  showPunctuation()
-  
-  let keepPlaying = confirm("Do you want to continue playing");
-  
-  if(keepPlaying === true) {
-    game();
-  } else {
-    alert("Goodbye");
-  }
-}
-game()
-}
+          if (keepCard === "no") {
+            return assingBingoCard();
+          }
+
+          alert("Invalid input");
+          return keep();
+        }
+      };
+
+      return keep();
+    };
+
+    const bingoCard = assingBingoCard();
+    let line1 = bingoCard.slice(0, 5);
+    let line2 = bingoCard.slice(5, 10);
+    let line3 = bingoCard.slice(10);
+    const changeToX = (element, index, randomNumber) => {
+      if (element === randomNumber) {
+        return "X";
+      }
+
+      return element;
+    };
+
+    const showCard = () =>
+      `\n${line1.join(" ")}\n${line2.join(" ")}\n${line3.join(" ")}`;
+
+    const correctNumbers = bingoCard;
+    const incorrectNumbers = [];
+    while (incorrectNumbers.length < 75) {
+      const randomNumber = Math.floor(Math.random() * 90) + 1;
+      if (
+        !correctNumbers.includes(randomNumber) &&
+        !incorrectNumbers.includes(randomNumber)
+      ) {
+        incorrectNumbers.push(randomNumber);
+      }
+    }
+
+    const getRandomNumber = () => {
+      const firstRandomNum = Math.random();
+      const secondRandomNum = Math.random();
+      const luck = firstRandomNum < secondRandomNum;
+
+      if (luck) {
+        const correctNum = [
+          correctNumbers[Math.floor(Math.random() * correctNumbers.length)],
+          true,
+        ];
+        correctNumbers.splice(correctNumbers.indexOf(correctNum[0]), 1);
+        return correctNum;
+      }
+
+      const incorrectNum = [
+        incorrectNumbers[Math.floor(Math.random() * incorrectNumbers.length)],
+        false,
+      ];
+      incorrectNumbers.splice(incorrectNumbers.indexOf(incorrectNum[0]), 1);
+      return incorrectNum;
+    };
+
+    let lineAlerted = false;
+
+    let turns = 1;
+    while (correctNumbers.length > 0) {
+      const number = getRandomNumber();
+      alert(`Turn ${turns}:\n NUMBER ${number[0]}`);
+      if (number[1]) {
+        line1 = line1.map((element) => changeToX(element, null, number[0]));
+        line2 = line2.map((element) => changeToX(element, null, number[0]));
+        line3 = line3.map((element) => changeToX(element, null, number[0]));
+        alert(
+          `Number ${
+            number[0]
+          } has been found and has therefore been marked with an X.\n${showCard()}`
+        );
+      } else {
+        alert(`Number ${number[0]} has not been found.\n${showCard()}`);
+      }
+
+      if (line1.every((x) => x === "X") && !lineAlerted) {
+        alert("Line!");
+        lineAlerted = true;
+      }
+
+      if (line2.every((x) => x === "X") && !lineAlerted) {
+        alert("Line!");
+        lineAlerted = true;
+      }
+
+      if (line3.every((x) => x === "X") && !lineAlerted) {
+        alert("Line!");
+        lineAlerted = true;
+      }
+
+      if (line1.concat(line2, line3).every((x) => x === "X")) {
+        alert("Bingo!");
+        break;
+      }
+
+      turns++;
+    }
+
+    alert(`You have completed this game in a total of ${turns} turns.`);
+    const punctuation = Math.trunc(((90 - turns) / 75) * 100);
+    alert(`Your score for this round is ${punctuation} points.`);
+    showPunctuation();
+
+    const keepPlaying = confirm("Do you want to continue playing");
+
+    if (keepPlaying === true) {
+      game();
+    } else {
+      alert("Goodbye");
+    }
+  };
+
+  game();
+};
 
 bingo();
--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,35 +1,39 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-implicit-coercion */
 function calculator() {
   let arr = [prompt(), prompt()];
   arr = arr.filter((m) => m !== null);
   arr = arr.map((x) => x.trim());
   arr = arr.filter((y) => y !== "");
-  let arrBoolean = arr.map((element) => Boolean(+element)),
-    check = arrBoolean.every((value) => value);
+  const arrBoolean = arr.map((element) => Boolean(+element));
+  const check = arrBoolean.every((value) => value);
 
   if (arr.length > 0) {
     if (arr.length > 1) {
       if (check) {
         arr = arr.map((index) => +index);
-        let results = [];
-        let signs = [" + ", " - ", " * ", " / "];
-        let sum = 0,
-          deduct = arr[0],
-          mult = 1,
-          div = +arr[0] == 1 ? 1 : +arr[0] * 2;
-        for (let add of arr) {
+        const results = [];
+        const signs = [" + ", " - ", " * ", " / "];
+        let sum = 0;
+        let deduct = arr[0];
+        let mult = 1;
+        let div = +arr[0] === 1 ? 1 : +arr[0] * 2;
+        for (const add of arr) {
           sum += add;
         }
 
-        for (i = 1; i < arr.length; i++) {
+        for (let i = 1; i < arr.length; i++) {
           deduct -= arr[i];
         }
 
-        for (let rept of arr) {
+        for (const rept of arr) {
           mult *= rept;
         }
-        for (let fract of arr) {
+
+        for (const fract of arr) {
           div /= fract;
         }
+
         results.push(sum, deduct, mult, div);
 
         for (let s = 0; s < signs.length; s++) {
@@ -43,13 +47,16 @@ function calculator() {
         console.log("Non numerical value");
       }
     } else {
-      check
+      const result = check
         ? Number.isInteger(Math.sqrt(arr[0]))
-          ? console.log(`√${arr[0]} = ${Math.sqrt(arr[0])}`)
-          : console.log(`√${arr[0]} = ${Math.sqrt(arr[0]).toFixed(3)}`)
-        : console.log("Non numerical value");
+          ? `√${arr[0]} = ${Math.sqrt(arr[0])}`
+          : `√${arr[0]} = ${Math.sqrt(arr[0]).toFixed(3)}`
+        : "Non numerical value";
+      console.log(result);
     }
   } else {
     console.log("Missing arguments");
   }
 }
+
+calculator();
